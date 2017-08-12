a955a52b989d8c5a26ab61025079b077b3ab4eb8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import lacksText from "./source.js"
+import keyChain from "./source"
 
 test(({ok, end}) => {
   ok(lacksText(".")("I have no period"))
