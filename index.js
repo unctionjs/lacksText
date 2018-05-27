e8e8a773b9e84039668ab6f44cd6ef70539416c2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import {test} from "ramda"
-import escapeStringRegexp from "escape-string-regexp"
-
-export default function lacksText (subset: StringType): Function {
-  const escaped = escapeStringRegexp(subset)
-  const pattern = test(new RegExp(escaped))
-
-  return function lacksTextSubset (set: StringType): boolean {
-    return !pattern(set)
-  }
+import { test } from "ramda";
+import escapeStringRegexp from "escape-string-regexp";
+export default function lacksText(subset) {
+  const escaped = escapeStringRegexp(subset);
+  const pattern = test(new RegExp(escaped));
+  return function lacksTextSubset(set) {
+    return !pattern(set);
+  };
 }
